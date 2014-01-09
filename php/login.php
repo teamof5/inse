3ff5d0073244904df569a6cfe8ee76172ed1209f
Krishna <testme@myport.ac.uk>
2014-01-09 13:05:10 +0000

    Create an authentication function

--- php/login.php
+++ php/login.php
@@ -1,19 +1,37 @@
 <?php
 
+/* 
+   09 Jan 2014
+   Ver 1. Created the page with a very few lines of code to receive data from form - K
+   Ver 2. Added a function $authenticate which will return true on valid email and password entered - K 
+*/
+
+
 // Getting username and password from the login page (index.html)
+
 $email = $GET_["username"];
 $password = $GET_["password"]
 
-// Checking if the login is successful
-// +++ INCLUDING DATABASE HERE +++
 
-if ($authentication) {
-	// Divert to the home page
+/* The following is dummy authentication function
+   Will be edited when databse is created */
+   
+function $authenticate($e,$p){
+	// +++ INCLUDING DATABASE HERE +++
+	If (strtolower($e)="test@test.com" & $p="test"){
+		return true;
+	}
+}
+
+
+// Checking if the login is successful
+
+if ($authenticate($email,$password)) {
+	// Diverting to home page
+	header ('Location: home.html');
 } else {
 	// Give out error message
 }
 
-// ++ Diverting until database is applied
-header ('Location: home.html');	
 
 ?>
